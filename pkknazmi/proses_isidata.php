1baf2ab3765cc736f1993f7269722c992c3aecd1
NazmiAF11 <114555528+NazmiAF11@users.noreply.github.com>
2023-02-14 07:57:24 +0700

    Delete pkknazmi directory

--- pkknazmi/proses_isidata.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "function/koneksi.php";
-$id = $_POST['id'];
-$tanggal = $_POST['tanggal'];
-$isi_laporan = $_POST['isi'];
-$status = $_POST['status'];
-$query = $koneksi->query("INSERT INTO pengaduan VALUES (null,'$id','$tanggal','$isi_laporan','$status')");
-$data = $query->fetch();
-header("location:catatan.php");
